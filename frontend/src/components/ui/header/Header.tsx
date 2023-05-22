470518b5263aadccf6dcf923c698314bc47e8766
Alex <gitlab.bec5f@aleeas.com>
2023-05-22 17:48:14 +0200

    change header and github

--- frontend/src/components/ui/header/Header.tsx
+++ frontend/src/components/ui/header/Header.tsx
@@ -1,22 +1,38 @@
 import { Link } from "react-router-dom";
-import { argentBankLogo, iconExit, styles } from "../../../assets";
+import { argentBankLogo, iconLogin, iconLogout, styles } from "../../../assets";
 import { Routes } from "../../../global";
+import { useAppSelector } from "../../../app/hooks";
+import { getToken } from "../../../features";
 
-const Header = () => (
-  <nav className={styles.mainNav}>
-    <Link className={styles.mainNavLogo} to={Routes.HOME}>
-      <img
-        className={styles.mainNavLogoImage}
-        src={argentBankLogo}
-        alt="Argent Bank Logo"
-      />
-      <h1 className={`${styles.srOnly}`}>Argent Bank</h1>
-    </Link>
+const Header = () => {
+  const token = useAppSelector(getToken);
 
-    <Link to={Routes.SIGNIN} className={styles.headerRight}>
-      <img src={iconExit} className={styles.headerLogo} alt="icon exit" />
-      Sign In
-    </Link>
-  </nav>
-);
+  const handleClick = () => {};
+
+  return (
+    <nav className={styles.mainNav}>
+      <Link className={styles.mainNavLogo} to={Routes.HOME}>
+        <img
+          className={styles.mainNavLogoImage}
+          src={argentBankLogo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className={`${styles.srOnly}`}>Argent Bank</h1>
+      </Link>
+
+      <Link
+        to={Routes.SIGNIN}
+        className={styles.headerRight}
+        onClick={handleClick}
+      >
+        <img
+          src={token ? iconLogout : iconLogin}
+          className={styles.headerLogo}
+          alt="icon login/logout"
+        />
+        {token ? "Logout" : "Login"}
+      </Link>
+    </nav>
+  );
+};
 export default Header;
